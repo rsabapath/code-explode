3a54cb04dd9c4aa2f88519cb25c09431ee7f9cd7
Alex Selesse <alex@selesse.com>
2013-03-07 22:16:57 -0500

    Remodel page (based on ruby.railstutorial.org).
    Create static pages (home, about, contact) and use responsive bootstrap.

--- website/app/helpers/application_helper.rb
+++ website/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = "Code-Explode"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
--- website/config/routes.rb
+++ website/config/routes.rb
@@ -1,5 +1,9 @@
 Website::Application.routes.draw do
-  root :to => 'projects#index'
+  root :to => 'static_pages#home'
+
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
   resources :projects
   # The priority is based upon order of creation:
   # first created -> highest priority.
